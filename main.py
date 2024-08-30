cf2bcff250d172e5f3a42c7131c66ce9a9125245
Andrew Friedman <andrew@cyberdreaming.net>
2024-08-29 17:51:00 -0700

    Implement book and available methods for the Hotel class.  Read id column as a string

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 import pandas as pd
 
-df = pd.read_csv('hotels.csv')
+df = pd.read_csv('hotels.csv', dtype={'id': str})
 
 
 class Hotel:
-    def __init__(self, id):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
 
     def book(self):
-        pass
+        # Book a hotel by changing availability to no
+        df.loc[df['id'] == self.hotel_id, 'availability'] = 'no'
+        df.to_csv('hotels.csv', index=False)
 
     def available(self):
-        pass
+        # Check if the hotel is available
+        return df.loc[df['id'] == self.hotel_id]['available'].squeeze() == 'yes'
 
 
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
         pass
+
     def generate(self):
         pass
 
 
 print(df)
-id = input('Enter the ID of the hotel')
-hotel = Hotel(id)
+hotel_ID = input('Enter the ID of the hotel')
+hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input('Enter your name: ')
-    reservation_ticket = ReservationTicket(hotel)
+    reservation_ticket = ReservationTicket(name, hotel)
     reservation_ticket.generate()
 else:
     print('Hotel is not available')
