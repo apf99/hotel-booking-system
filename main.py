7773d106342eb8d125e2e5d902a203a8591fad7b
Andrew Friedman <andrew@cyberdreaming.net>
2024-08-29 18:54:43 -0700

    Implement the methods for the reservation class.  Add a name attribute for the Hotel class.

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ df = pd.read_csv('hotels.csv', dtype={'id': str})
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df['id'] == hotel_id]['name'].squeeze()
 
     def book(self):
         # Book a hotel by changing availability to no
-        df.loc[df['id'] == self.hotel_id, 'availability'] = 'no'
+        df.loc[df['id'] == self.hotel_id, 'available'] = 'no'
         df.to_csv('hotels.csv', index=False)
 
     def available(self):
@@ -19,19 +20,27 @@ class Hotel:
 
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
-        pass
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
     def generate(self):
-        pass
+        content = f"""
+        Thank you for your reservation!
+        Here are your booking data:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel.name}
+        """
+        return content
 
 
 print(df)
-hotel_ID = input('Enter the ID of the hotel')
+hotel_ID = input('Enter the ID of the hotel: ')
 hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input('Enter your name: ')
-    reservation_ticket = ReservationTicket(name, hotel)
-    reservation_ticket.generate()
+    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+    print(reservation_ticket.generate())
 else:
     print('Hotel is not available')
+
